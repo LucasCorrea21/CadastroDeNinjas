b332472c334149cec1acfe07f269977538464f2d
Lucas <lucasdesouzacorrea@gmail.com>
2026-06-09 22:35:13 -0300

    feat: Inclui o método put porém não funciona ainda corretamente, preciso trabalhar em alterações parciais, pois está alterando tudo além do que eu realmente quero alterar

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -39,9 +39,9 @@ public class NinjaController {
     }
 
     // Alterar dados dos Ninjas
-    @PutMapping("/alterarID")
-    public String alterarNinjaPorID(){
-        return "Altera Ninja Ninjas Por ID";
+    @PutMapping("/alterar/{id}")
+    public NinjaModel alterarNinjaPorID(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     // Deletar Ninjas
